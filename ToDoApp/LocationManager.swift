c7fe065b58676d365799734ba5ca75f063961af7
Kate Lee <145916607+kaitlynhlee@users.noreply.github.com>
2025-05-14 21:27:38 -0700

    added option of adding location/distance to tasks

---
+++ ToDoApp/LocationManager.swift
@@ -0,0 +1,25 @@
+//
+//  LocationManager.swift
+//  ToDoApp
+//
+//  Created by Kaitlyn Lee on 5/14/25.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    @Published var currentLocation: CLLocation?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.last
+    }
+}
--- ToDoApp/ToDoItem.swift
+++ ToDoApp/ToDoItem.swift
@@ -6,10 +6,21 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct ToDoItem: Identifiable, Codable {
     var id = UUID()
     var title: String
     var isCompleted: Bool = false
     var priority: Int = 1
+    
+    var latitude: Double?
+    var longitude: Double?
+    
+    var location: CLLocation? {
+        if let lat = latitude, let lon = longitude {
+            return CLLocation(latitude: lat, longitude: lon)
+        }
+        return nil
+    }
 }
